766001aa3fe8d6aba8fcf1996957aae43631cba0
alivolley <alicryptovolley@gmail.com>
2023-10-18 18:13:36 +0330

    home : banner changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
+// Assets
+import bannerPic from '../assets/images/banner.png';
+
 export default function Home() {
-   return <div />;
+   return (
+      <div>
+         <div className="customSm:bg-customOrange customSm:p-6 customMd:p-[60px]">
+            <Image src={bannerPic} alt="banner" className="h-full w-full" />
+         </div>
+      </div>
+   );
 }
