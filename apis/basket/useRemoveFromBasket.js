fae7911e3b934800381d60a4b23c60fbe3901625
alivolley <alicryptovolley@gmail.com>
2023-11-29 18:53:05 +0330

    add to basket and remove from basket alerts set

--- apis/basket/useRemoveFromBasket.js
+++ apis/basket/useRemoveFromBasket.js
@@ -1,11 +1,22 @@
 import useSWRMutation from 'swr/mutation';
 import { useSWRConfig } from 'swr';
+import { toast } from 'react-toastify';
 import axiosInstance from '@/configs/axiosInstance';
 
 const useRemoveFromBasket = () => {
    const { mutate } = useSWRConfig();
    return useSWRMutation('restaurant/cart/get_update/', (url, data) =>
       axiosInstance.patch(url, data.arg).then(res => {
+         if (data?.arg?.food_count === 0) {
+            toast.success('محصول از سبد خرید حذف شد', {
+               style: {
+                  direction: 'rtl',
+                  fontFamily: 'rokhRegular',
+                  lineHeight: '25px',
+               },
+               autoClose: 5000,
+            });
+         }
          mutate('restaurant/cart/get_update/', res.data);
          return res.data;
       })
